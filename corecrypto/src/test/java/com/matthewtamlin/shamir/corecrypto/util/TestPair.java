730e0f5a90e4d3fda6c4b12caeed2241403caed0
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-01-16 19:23:59 +1030

    test(corecrypto): updated test for Pair class to check null cases

--- corecrypto/src/test/java/com/matthewtamlin/shamir/corecrypto/util/TestPair.java
+++ corecrypto/src/test/java/com/matthewtamlin/shamir/corecrypto/util/TestPair.java
@@ -1,26 +1,28 @@
 package com.matthewtamlin.shamir.corecrypto.util;
 
 import org.junit.Test;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.Matchers.is;
 
 /**
  * Unit tests for the {@link Pair} class.
  */
 public class TestPair {
-    @Test
-    public void testInstantiateAndGet_nullValues() {
-        final Pair<String, Integer> pair = Pair.create(null, null);
-        
-        assertThat(pair.getKey(), is(nullValue()));
-        assertThat(pair.getValue(), is(nullValue()));
+    @Test(expected = NullPointerException.class)
+    public void testInstantiate_nullKey() {
+        Pair.create(null, 123456);
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void testInstantiate_nullValue() {
+        Pair.create("test", null);
     }
     
     @Test
-    public void testInstantiateAndGet_nonNullValues() {
+    public void testInstantiateAndGet() {
         final Pair<String, Integer> pair = Pair.create("test", 123456);
-    
+        
         assertThat(pair.getKey(), is("test"));
         assertThat(pair.getValue(), is(123456));
     }
