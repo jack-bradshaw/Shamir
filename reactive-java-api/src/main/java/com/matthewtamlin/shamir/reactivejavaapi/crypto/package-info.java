858a903e9d932534bf76d933d20239edb9b63299
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-01-19 10:01:30 +1030

    refactor(reactive-java-api): moved classes from root package to crypto package

--- reactive-java-api/src/main/java/com/matthewtamlin/shamir/reactivejavaapi/crypto/package-info.java
+++ reactive-java-api/src/main/java/com/matthewtamlin/shamir/reactivejavaapi/crypto/package-info.java
@@ -2,4 +2,4 @@
  * Contains classes that wrap the classes in {@link com.matthewtamlin.shamir} to provide a reactive API for Shamir's
  * Secret Sharing.
  */
-package com.matthewtamlin.shamir.reactivejavaapi;
+package com.matthewtamlin.shamir.reactivejavaapi.crypto;
