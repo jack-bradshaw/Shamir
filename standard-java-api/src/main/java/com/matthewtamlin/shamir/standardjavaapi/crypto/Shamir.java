fed710b250192e407292e1e66ea001be6575d4da
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-01-20 13:03:14 +1030

    refactor(standard-java-api): refactored Shamir class to use RxShamir internally

--- standard-java-api/src/main/java/com/matthewtamlin/shamir/standardjavaapi/crypto/Shamir.java
+++ standard-java-api/src/main/java/com/matthewtamlin/shamir/standardjavaapi/crypto/Shamir.java
@@ -1,16 +1,16 @@
 package com.matthewtamlin.shamir.standardjavaapi.crypto;
 
-import com.matthewtamlin.shamir.commonslibrary.math.Polynomial;
 import com.matthewtamlin.shamir.commonslibrary.model.CreationScheme;
 import com.matthewtamlin.shamir.commonslibrary.model.RecoveryScheme;
 import com.matthewtamlin.shamir.commonslibrary.model.Share;
-import com.matthewtamlin.shamir.commonslibrary.util.Pair;
+import com.matthewtamlin.shamir.reactivejavaapi.crypto.RxShamir;
 import io.reactivex.Observable;
 
 import javax.annotation.Nonnull;
 import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkEachElementIsNotNull;
@@ -24,7 +24,7 @@ import static java.math.BigInteger.ZERO;
  * a finite field to prevent geometric attacks.
  */
 public class Shamir {
-    private final SecureRandom random;
+    private final RxShamir rxShamir;
     
     /**
      * Constructs a new Shamir. The provided random source is used in cryptographic operations, so ensure the source is
@@ -34,7 +34,9 @@ public class Shamir {
      *         the random source for use in cryptographic operations, not null
      */
     public Shamir(@Nonnull final SecureRandom random) {
-        this.random = checkNotNull(random, "\'random\' must not be null.");
+        checkNotNull(random, "\'random\' must not be null.");
+        
+        rxShamir = RxShamir.create(random);
     }
     
     /**
@@ -65,36 +67,9 @@ public class Shamir {
      */
     @Nonnull
     public Set<Share> createShares(@Nonnull final BigInteger secret, @Nonnull final CreationScheme creationScheme) {
-        checkNotNull(secret, "\'secret\' must not be null.");
-        checkNotNull(creationScheme, "\'creationScheme\' must not be null.");
-        
-        checkArgument(
-                creationScheme.getPrime().compareTo(secret) > 0,
-                "\'secret\' must be less than the prime defined in \'creationScheme\'.");
-        
-        final Map<Integer, BigInteger> coefficients = Observable
-                .range(1, creationScheme.getRequiredShareCount() - 1)
-                .map(index -> Pair.create(index, createRandomCoefficient(creationScheme.getPrime())))
-                .collectInto(new HashMap<Integer, BigInteger>(), (map, pair) -> map.put(pair.getKey(), pair.getValue()))
-                .blockingGet();
-        
-        coefficients.put(0, secret);
-        
-        final Polynomial polynomial = new Polynomial(coefficients);
-        
-        return Observable
-                .range(1, creationScheme.getTotalShareCount())
-                .map(index -> Pair.create(
-                        index,
-                        polynomial
-                                .evaluateAt(BigInteger.valueOf(index))
-                                .mod(creationScheme.getPrime())))
-                .map(pair -> Share
-                        .builder()
-                        .setIndex(pair.getKey())
-                        .setValue(pair.getValue())
-                        .build())
-                .collectInto(new HashSet<Share>(), HashSet::add)
+        return rxShamir
+                .createShares(secret, creationScheme)
+                .collectInto(new HashSet<Share>(), Set::add)
                 .blockingGet();
     }
     
@@ -123,92 +98,6 @@ public class Shamir {
      */
     @Nonnull
     public BigInteger recoverSecret(@Nonnull final Set<Share> shares, @Nonnull final RecoveryScheme recoveryScheme) {
-        checkNotNull(shares, "\'shares\' must not be null.");
-        checkEachElementIsNotNull(shares, "\'shares\' must not contain null.");
-        checkNotNull(recoveryScheme, "\'recoveryScheme\' must not be null.");
-        
-        validateShares(shares, recoveryScheme);
-        
-        final List<Share> orderedShares = new ArrayList<>(shares);
-        orderedShares.sort(Comparator.comparing(Share::getIndex));
-        
-        BigInteger secret = BigInteger.ZERO;
-        
-        for (final Share outerShare : shares) {
-            BigInteger outerWorking = ONE;
-            
-            for (final Share innerShare : shares) {
-                if (outerShare.equals(innerShare)) {
-                    continue;
-                }
-                
-                final BigInteger innerWorking = innerShare
-                        .getIndex()
-                        .subtract(outerShare.getIndex())
-                        .modInverse(recoveryScheme.getPrime());
-                
-                outerWorking = outerWorking.multiply(
-                        innerShare
-                                .getIndex()
-                                .multiply(innerWorking));
-            }
-            
-            outerWorking = outerWorking.multiply(outerShare.getValue());
-            
-            secret = secret.add(outerWorking);
-        }
-        
-        secret = secret.mod(recoveryScheme.getPrime());
-        
-        return secret;
-    }
-    
-    private void validateShares(final Set<Share> shares, final RecoveryScheme recoveryScheme) {
-        if (shares.size() < recoveryScheme.getRequiredShareCount()) {
-            throw new IllegalArgumentException(format(
-                    "The recovery scheme requires at least %1$s shares, but only %2$s shares were provided.",
-                    recoveryScheme.getRequiredShareCount(),
-                    shares.size()));
-        }
-        
-        final boolean allSharesHaveDistinctIndex = Observable
-                .fromIterable(shares)
-                .distinct(Share::getIndex)
-                .count()
-                .map(count -> count == shares.size())
-                .blockingGet();
-        
-        if (!allSharesHaveDistinctIndex) {
-            throw new IllegalArgumentException("Every share must have a distinct index.");
-        }
-        
-        for (final Share s : shares) {
-            if (s.getIndex().compareTo(recoveryScheme.getPrime()) >= 0) {
-                throw new IllegalArgumentException("The index of every share must be less than the prime.");
-            }
-            
-            if (s.getValue().compareTo(recoveryScheme.getPrime()) >= 0) {
-                throw new IllegalArgumentException("The value of every share must be less than the prime.");
-            }
-        }
-    }
-    
-    private BigInteger createRandomCoefficient(final BigInteger prime) {
-        checkArgument(prime.compareTo(ONE) > 0, "\'prime\' must be greater than 1.");
-        
-        // Need to loop until the generated coefficient belongs to (0, limit)
-        while (true) {
-            final BigInteger randomCoefficient = new BigInteger(prime.bitLength(), random);
-            
-            if (randomCoefficient.compareTo(prime) >= 0) {
-                continue;
-            }
-            
-            if (randomCoefficient.compareTo(ZERO) == 0) {
-                continue;
-            }
-            
-            return randomCoefficient;
-        }
+        return rxShamir.recoverSecret(shares, recoveryScheme).blockingGet();
     }
 }
