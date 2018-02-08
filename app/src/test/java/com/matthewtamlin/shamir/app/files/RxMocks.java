7ace0ff9347f11e96c0d57f1a600c64a36e7edcd
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-02-08 17:56:56 +1030

    test(rxutilities): create tests for files components

---
+++ app/src/test/java/com/matthewtamlin/shamir/app/files/RxMocks.java
@@ -0,0 +1,27 @@
+package com.matthewtamlin.shamir.app.files;
+
+import com.matthewtamlin.shamir.app.files.RxFiles;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RxMocks {
+  public static RxFiles createMockRxFiles() {
+    final RxFiles rxFiles = mock(RxFiles.class);
+    
+    when(rxFiles.isDirectory(any())).thenReturn(Single.never());
+    when(rxFiles.isFile(any())).thenReturn(Single.never());
+    when(rxFiles.exists(any())).thenReturn(Single.never());
+    when(rxFiles.createDirectory(any())).thenReturn(Completable.never());
+    when(rxFiles.createNewFile(any())).thenReturn(Completable.never());
+    when(rxFiles.readStringFromFile(any(), any())).thenReturn(Single.never());
+    when(rxFiles.writeStringToFile(any(), any(), any())).thenReturn(Completable.never());
+    when(rxFiles.getFilesInDirectory(any())).thenReturn(Observable.never());
+    
+    return rxFiles;
+  }
+}
